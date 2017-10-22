4ad70a60d6ca55f0a0adaeacaed4fb62935e3d5f
140221059 <32795237+140221059@users.noreply.github.com>
2017-10-22 23:02:54 +0100

    alterei isto

--- VSHelloWorld_140221059/Program.cs
+++ VSHelloWorld_140221059/Program.cs
@@ -14,8 +14,7 @@ namespace VSHelloWorld_140221059
     {
         public static void Main(string[] args)
         {
-            //comentario
-            //outro comentario
+            //Miguel Parro
             BuildWebHost(args).Run();
         }
 
